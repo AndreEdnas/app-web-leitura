699f6f2c86d17b1af7b4f35f916b955112ef450a
AndreEdnas <andre@ednas.pt>
2025-10-01 09:53:51 +0100

    Remover pasta bats do repositório e adicionar ao .gitignore

--- bats/start_api.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM ----------------------------------------
-REM Inicia a API Node.js minimizado
-REM ----------------------------------------
-start /min "" node "C:\EDNAS\APP\app-web-leitura\backend\server.js"
